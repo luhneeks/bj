b3bb8e15dc192d0b22ce31af4fa1ee213a76335f
neeks <luhneeks96@gmail.com>
2024-11-15 22:02:36 -0800

    added notes on the functions I need to create

---
+++ main.js
@@ -0,0 +1,11 @@
+let deck;
+let count = [];
+
+
+console.log(count)
+
+// need a function that generates the deck
+// need a function that starts a new count
+// need a function that checks for BJ
+// need a function that checks if its over 21/bust
+// need to think about soft and ace dynamic into code
